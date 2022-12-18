68a3d6abbc60632797bb1edad9a659dd5f71da3f
Vincent-Turel <vincetl74@gmail.com>
2022-12-18 09:31:34 +0100

    scan bad repo

--- main.py
+++ main.py
@@ -1,66 +1,111 @@
+import os
 import subprocess
 import requests
 
 
-def download_repositories(number: int) -> None:
-    print(f'Downloading {number} repositories from GitHub...')
+def download_repositories(number: int, page_number: int) -> None:
     token = "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"
-    url = "https://api.github.com/search/repositories?q=language:jupyter-notebook&order=desc"
+    url = "https://api.github.com/search/repositories?q=language:jupyter-notebook&order=desc&page=" + str(page_number)
     headers = {
         'Accept': 'application/vnd.github.preview.text-match+json',
         'Authorization': 'Bearer ' + token,
     }
 
     resp = requests.get(url, headers=headers)
-    print(resp.status_code)
 
-    urls = [item['html_url'] for item in resp.json()['items'][:number]]
-    print(urls)
+    # Get urls of one of each three repository
+    urls = []
+    for i in range(0, number * 3, 3):
+        urls.append(resp.json()['items'][i]['html_url'])
 
     for url in urls:
         subprocess.run(['git', 'clone', '--depth', '1', url, './repositories/' + url.split('/')[-1]])
 
-    print(f'Repositories downloaded !')
+
+def download_good_repositories(number: int) -> None:
+    print(f'Downloading {number} repositories from GitHub...')
+    download_repositories(number, 1)
+    print('Good repositories downloaded !')
+
+
+def download_bad_repositories(number: int) -> None:
+    print(f'Downloading {number} repositories from GitHub...')
+    download_repositories(number, 30)
+    print('Bad repositories downloaded !')
 
 
 def extract_jupyter_notebooks() -> None:
     print("Extracting jupyter notebooks from repositories...")
-    subprocess.run(['mv', './repositories/**/*.ipynb', './jupyter-notebooks/'])
+    subprocess.run(['find', './repositories/', '-name', '*.ipynb', '-exec', 'cp', '{}', './jupyter-notebooks/', ';'])
     print("Jupyter notebooks extracted !")
 
 
-def convert_notebooks_to_python():
-    print("Converting jupyter notebooks to python scripts...")
-    subprocess.run(['jupyter', 'nbconvert',
-                    './jupyter-notebooks/*.ipynb',
-                    '--to', 'script',
-                    '--output-dir', './python-scripts/'])
+def convert_notebooks_to_python() -> None:
+    print("Converting jupyter notebooks to scripts...")
+    try:
+        subprocess.run(['jupyter', 'nbconvert',
+                        './jupyter-notebooks/*.ipynb',
+                        '--to', 'script',
+                        '--output-dir', './python-scripts/'])
+    except (Exception, RuntimeError):
+        print("Notebook validation failed !")
 
-    print("Jupyter notebooks converted to python scripts !")
+    print("Jupyter notebooks converted to scripts !")
+
+    print("Removing non python files...")
+    for item in os.listdir('./python-scripts/'):
+        if not item.endswith(".py"):
+            os.remove(os.path.join('./python-scripts/', item))
+    print("Non python files removed !")
 
 
-def scan_python_files():
+def scan_python_files() -> None:
     print("Scanning python files for code quality...")
-    subprocess.run(['./sonar-scanner/bin/sonar-scanner',
-                    '-Dsonar.projectKey=rimel',
-                    '-Dsonar.sources=./python-scripts',
-                    '-Dsonar.host.url=http://127.0.0.1:9000',
-                    '-Dsonar.login=sqp_eca496bcb5a1a655ee3f56ef498e489c402012e7'])
+    for file in os.listdir('./python-scripts/'):
+        subprocess.run(['curl', '-X', 'POST', 'http://localhost:9000/api/projects/create?name=' + file[:-3]
+                        + '&project=' + file[:-3]])
+
+    for file in os.listdir('./python-scripts/'):
+        subprocess.run(['./sonar-scanner/bin/sonar-scanner',
+                        '-Dsonar.projectKey=' + file[:-3],
+                        '-Dsonar.sources=./python-scripts/' + file,
+                        '-Dsonar.host.url=http://localhost:9000',
+                        '-Dsonar.login=sqa_cef5900dd30278f974d984b3f3f2d5cb7a8beb4b'])
+    print("Python files scanned !")
+
+
+def scan_bad_python_files() -> None:
+    print("Scanning python files for code quality...")
+    for file in os.listdir('./python-scripts/'):
+        subprocess.run(['curl', '-X', 'POST', 'http://localhost:9000/api/projects/create?name=' + file[:-3]
+                        + '&project=' + file[:-3]])
+
+    for file in os.listdir('./python-scripts/'):
+        subprocess.run(['./sonar-scanner/bin/sonar-scanner',
+                        '-Dsonar.projectKey=' + file[:-3],
+                        '-Dsonar.sources=./python-scripts/' + file,
+                        '-Dsonar.host.url=http://localhost:9001',
+                        '-Dsonar.login=sqa_f211bc159d8bb7188d22b149ed37777ddeeabdc0'])
     print("Python files scanned !")
 
 
 def delete_files() -> None:
-    print("Deleting files from folders { ./repositories/, ./jupyter-notebooks/, ./python-scripts/ }")
-    subprocess.run(['rm', '-rf', './repositories/'])
-    subprocess.run(['rm', '-rf', './jupyter-notebooks/'])
-    subprocess.run(['rm', '-rf', './python-scripts/'])
+    print("Deleting files from folders { ./jupyter-notebooks/, ./python-scripts/ }")
+
+    for file in os.listdir('./python-scripts/'):
+        os.remove('./python-scripts/' + file)
+
+    for file in os.listdir('./jupyter-notebooks/'):
+        os.remove('./jupyter-notebooks/' + file)
+
     print("All files deleted !")
 
 
 if __name__ == '__main__':
-    download_repositories(15)
+    # download_good_repositories(10)
+    download_bad_repositories(10)
     extract_jupyter_notebooks()
     convert_notebooks_to_python()
-    scan_python_files()
+    # scan_python_files()
+    scan_bad_python_files()
     delete_files()
-
